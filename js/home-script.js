4f465daff77f41a7cb38e720fbbe08259bc09a52
Testing01111 <abhijeetjain9b9203@gmail.com>
2025-01-27 12:51:45 +0530

    Add files via upload

---
+++ js/home-script.js
@@ -0,0 +1,34 @@
+let shopButton = document.body.querySelector(".content-button");
+shopButton.addEventListener("click",()=>{
+    window.scrollTo({
+        top:document.getElementById("grid-focus").offsetTop,
+        behavior: "smooth"
+    })
+})
+let shopTab = document.body.querySelector(".shop-scroll");
+shopTab.addEventListener("click",()=>{
+    window.scrollTo({
+        top:document.getElementById("grid-focus").offsetTop,
+        behavior: "smooth"
+    })
+})
+let arrivalscroll = document.body.querySelector(".arrivalscroll");
+arrivalscroll.addEventListener("click",()=>{
+    window.scrollTo({
+        top:document.getElementById("new-arrival-scroll").offsetTop,
+        behavior: "smooth"
+    })
+})
+
+let redirect=Array.from(document.body.querySelectorAll(".redirect"));
+
+for(ele of redirect){
+    ele.addEventListener("click",()=>{
+        window.location.href="/html/product_details.html";
+    })
+}
+
+let logo=document.body.querySelector(".logo");
+logo.addEventListener("click",()=>{
+    window.location.href="/html/home.html";
+})
---
+++ js/product_details.js
@@ -0,0 +1,4 @@
+let logo=document.body.querySelector(".logo");
+logo.addEventListener("click",()=>{
+    window.location.href="/html/home.html";
+})
