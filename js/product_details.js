010b2797ae702c61e3bb074865101fc6a69ca02e
Testing01111 <abhijeetjain9b9203@gmail.com>
2025-01-27 14:17:49 +0530

    Update product_details.js

--- js/product_details.js
+++ js/product_details.js
@@ -1,4 +1,4 @@
 let logo=document.body.querySelector(".logo");
 logo.addEventListener("click",()=>{
-    window.location.href="/Basic_Ecommerce/html/home.html";
+    window.location.href="https://testing01111.github.io/Basic_Ecommerce/html/home.html";
 })
